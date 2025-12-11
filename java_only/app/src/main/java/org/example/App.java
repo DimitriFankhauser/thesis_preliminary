2539e502d704024216319d191e871220d86acc8f
Dimitri Fankhauser <dimitri.fankhauser@proton.me>
2025-12-11 15:11:42 +0100

    ref: start with plain java

--- java_only/app/src/main/java/org/example/App.java
+++ java_only/app/src/main/java/org/example/App.java
@@ -9,6 +9,31 @@ public class App {
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+
+        String configuration = "pkcs11.cfg";
+
+        Provider sunPkcs11 = Security.getProvider("SunPKCS11");
+        Provider pkcsImplementation = sunPkcs11.configure(configuration);
+
+        if (pkcsImplementation != null) {
+            Log.info("PKCSImplementation not null");
+        }
+
+        Security.addProvider(pkcsImplementation);
+
+        KeyStore hsmKeyStore = KeyStore.getInstance("PKCS11", pkcsImplementation);
+        String userPin = "98765432"; // The pin to unlock the HSM-TOKEN
+
+        hsmKeyStore.load(null, userPin.toCharArray());
+        String keyID = "2222"; // The key identifier or alias
+        String keyPin = ""; // Optional pin to unlock the key => not needed
+
+        //this is null for some reason
+        Key k= hsmKeyStore.getKey(keyID, userPin.toCharArray());
+
+        Log.info("Key is: "+ k);
+
+
+
+        PublicKey publicKey= (PublicKey) hsmKeyStore.getKey(keyID, null);
 }
