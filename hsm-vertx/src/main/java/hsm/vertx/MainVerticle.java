d8da19ad82f2f2bfc7e208e37e96e746e7a7da23
Dimitri Fankhauser <135702874+DimitriFankhauser@users.noreply.github.com>
2026-02-23 11:53:52 +0100

    feat: vertx-version

--- hsm-vertx/src/main/java/hsm/vertx/MainVerticle.java
+++ hsm-vertx/src/main/java/hsm/vertx/MainVerticle.java
@@ -3,26 +3,32 @@ package hsm.vertx;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) {
     Router router = Router.router(vertx);
-    router.get("/question").handler(ctx -> {
-      ctx.response()
-        .putHeader("content-type", "text/plain")
-        .end("What is your favourite food?");
+    router.route().handler(BodyHandler.create());
+    RsaUtil rsaUtil= new RsaUtil();
+
+    router.post("/crypto/encryptRSA").handler(rc -> {
+      EncryptableMessage e= rc.body().asJsonObject().mapTo(EncryptableMessage.class);
+      String ciphertext = rsaUtil.encrypt(e.message);
+      rc.response().putHeader("content-type","text/plain").end(ciphertext);
     });
 
-    router.post("/question").handler(ctx -> {
-      ctx.response()
-        .putHeader("content-type", "text/plain")
-        .end("You just sent a POST Request");
+    router.post("/crypto/decryptRSA").handler(rc -> {
+      EncryptableMessage e= rc.body().asJsonObject().mapTo(EncryptableMessage.class);
+      String cleartext = rsaUtil.decrypt(e.message);
+      rc.response().putHeader("content-type","text/plain").end("Cleartext:  "+cleartext);
     });
 
     vertx.createHttpServer()
       .requestHandler(router)
-      .listen(8080);
+      .listen(8080)
+      .onSuccess(server -> startPromise.complete())
+      .onFailure(startPromise::fail);
   }
 }
--- hsm-vertx/src/main/java/hsm/vertx/RsaUtil.java
+++ hsm-vertx/src/main/java/hsm/vertx/RsaUtil.java
@@ -15,74 +15,83 @@ import java.security.interfaces.RSAPublicKey;
 import java.util.Base64;
 
 public class RsaUtil {
-    private Logger Log = LoggerFactory.getLogger(RsaUtil.class);
-    private String userPin;
-    private String keyAlias;
-    private KeyStore hsmKeyStore;
+  private Logger Log = LoggerFactory.getLogger(RsaUtil.class);
+  private String userPin;
+  private String keyAlias;
+  private KeyStore hsmKeyStore;
 
-    public RsaUtil() {
-        this.userPin = "123456789"; // The pin to unlock the HSM-TOKEN
-        this.keyAlias = "rsaGenesis";
-        setup();
+
+  RsaUtil(){
+    this.userPin = "123456789"; // The pin to unlock the HSM-TOKEN
+    this.keyAlias = "rsaGenesis";
+    setup();
+  };
+
+  public Provider ensurePKCSImplementation(String configfilePath){
+    Provider sunPkcs11 = Security.getProvider("SunPKCS11");
+    Provider pkcsImplementation = sunPkcs11.configure(configfilePath);
+
+    if (pkcsImplementation != null) {
+      Log.info("PKCS implementation is not null");
+      Security.addProvider(pkcsImplementation);
+      return pkcsImplementation;
+    }
+    throw new RuntimeException("No PKCS Implementation found");
+  }
+
+  private void setup() {
+    this.Log = LoggerFactory.getLogger(RsaUtil.class);
+    try {
+      String configFilePath = "/home/dimitri/Documents/HSLU/BAA/thesis_preliminary/java_only/pkcs11.cfg";
+      Provider pkcsImplementation=ensurePKCSImplementation(configFilePath);
+      this.hsmKeyStore = KeyStore.getInstance("PKCS11", pkcsImplementation);
+      // load keystore and log in
+      this.hsmKeyStore.load(null, userPin.toCharArray());
+    } catch (IOException | NoSuchAlgorithmException | CertificateException | KeyStoreException e) {
+      Log.error(e.getClass() + e.getMessage());
     }
 
-    public RsaUtil(String configFilePath, String userPin, String keyID, String keyAlias) {
-        this.userPin = userPin;
-        this.keyAlias = keyAlias;
-        setup();
+  }
+
+  public String encrypt(String message){
+    Cipher encryptCipher = null;
+    try {
+      RSAPublicKey publicKey = (RSAPublicKey) hsmKeyStore.getCertificate(this.keyAlias).getPublicKey();
+      encryptCipher = Cipher.getInstance("RSA");
+      encryptCipher.init(Cipher.ENCRYPT_MODE, publicKey);
+      byte[] secretMessageBytes = message.getBytes(StandardCharsets.UTF_8);
+      byte[] encryptedMessageBytes = encryptCipher.doFinal(secretMessageBytes);
+
+      return Base64.getEncoder().encodeToString(encryptedMessageBytes);
+    } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException |
+             BadPaddingException | KeyStoreException e) {
+      Log.error(e.getMessage());
+      return "";
     }
+  }
 
-    public String encrypt(String message) {
+  public String decrypt(String ciphertext){
+    Cipher decryptCipher=null;
 
-        Cipher encryptCipher = null;
-        try {
-            RSAPublicKey publicKey = (RSAPublicKey) hsmKeyStore.getCertificate(this.keyAlias).getPublicKey();
-            encryptCipher = Cipher.getInstance("RSA");
-            encryptCipher.init(Cipher.ENCRYPT_MODE, publicKey);
+    try {
+      PrivateKey privateKey = (PrivateKey) hsmKeyStore.getKey(this.keyAlias, userPin.toCharArray());
+      decryptCipher=Cipher.getInstance("RSA");
+      decryptCipher.init(Cipher.DECRYPT_MODE,privateKey);
+      // decode from string back to byte array
+      byte[] secretMessageBytes = ciphertext.getBytes(StandardCharsets.UTF_8);
 
-            byte[] secretMessageBytes = message.getBytes(StandardCharsets.UTF_8);
-            byte[] encryptedMessageBytes = encryptCipher.doFinal(secretMessageBytes);
+      byte[] encryptedMessageBytes=Base64.getDecoder().decode(secretMessageBytes);
 
-            return Base64.getEncoder().encodeToString(encryptedMessageBytes);
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException
-                | BadPaddingException | KeyStoreException e) {
-            this.Log.error(e.getClass() + e.getMessage());
-            return "";
-        }
+      byte[] decryptedMessageBytes = decryptCipher.doFinal(encryptedMessageBytes);
+      String decryptedMessage = new String(decryptedMessageBytes, StandardCharsets.UTF_8);
+      return decryptedMessage;
+
+    } catch (NoSuchPaddingException | NoSuchAlgorithmException | IllegalBlockSizeException | BadPaddingException |
+             InvalidKeyException | KeyStoreException | UnrecoverableKeyException e) {
+      Log.error(e.getMessage());
+      return "";
     }
+  }
 
-    public String decrypt(String ciphertext) {
 
-        Cipher decryptCipher = null;
-        try {
-            PrivateKey privateKey = (PrivateKey) hsmKeyStore.getKey(this.keyAlias, userPin.toCharArray());
-            decryptCipher = Cipher.getInstance("RSA", this.hsmKeyStore.getProvider());
-            decryptCipher.init(Cipher.DECRYPT_MODE, privateKey);
-            // decode from string back to byte array
-            byte[] secretMessageBytes = ciphertext.getBytes(StandardCharsets.UTF_8);
-
-            byte[] encryptedMessageBytes = Base64.getDecoder().decode(secretMessageBytes);
-
-            byte[] decryptedMessageBytes = decryptCipher.doFinal(encryptedMessageBytes);
-            String decryptedMessage = new String(decryptedMessageBytes, StandardCharsets.UTF_8);
-            return decryptedMessage;
-
-        } catch (NoSuchPaddingException | NoSuchAlgorithmException | IllegalBlockSizeException | BadPaddingException
-                | UnrecoverableKeyException | KeyStoreException | InvalidKeyException e) {
-            this.Log.error(e.getClass() + e.getMessage());
-            return "";
-        }
-    }
-
-    private void setup() {
-        this.Log = LoggerFactory.getLogger(RsaUtil.class);
-        try {
-            this.hsmKeyStore = KeyStore.getInstance("PKCS11");
-            // load keystore and log in
-            this.hsmKeyStore.load(null, userPin.toCharArray());
-        } catch (IOException | NoSuchAlgorithmException | CertificateException | KeyStoreException e) {
-            Log.error(e.getClass() + e.getMessage());
-        }
-
-    }
 }
