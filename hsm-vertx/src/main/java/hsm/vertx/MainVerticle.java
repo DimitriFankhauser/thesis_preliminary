5c8cd4a15406292c9b7d79b591402d20ee3ae812
Dimitri Fankhauser <135702874+DimitriFankhauser@users.noreply.github.com>
2026-03-01 11:56:10 +0100

    config: start config from JSON-file

--- hsm-vertx/src/main/java/hsm/vertx/MainVerticle.java
+++ hsm-vertx/src/main/java/hsm/vertx/MainVerticle.java
@@ -3,68 +3,51 @@ package hsm.vertx;
 import io.vertx.config.ConfigRetriever;
 import io.vertx.config.ConfigRetrieverOptions;
 import io.vertx.config.ConfigStoreOptions;
-import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
+import io.vertx.core.VerticleBase;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 
-public class MainVerticle extends AbstractVerticle {
+public class MainVerticle extends VerticleBase {
 
   @Override
-  public void start(Promise<Void> startPromise) {
-    ConfigStoreOptions propertyWithHierarchical = new ConfigStoreOptions()
-      .setFormat("properties")
-      .setType("file")
-      .setConfig(new JsonObject().put("path", "application.properties").put("hierarchical", true)
-      );
-    ConfigRetrieverOptions options = new ConfigRetrieverOptions()
-      .addStore(propertyWithHierarchical);
-
+  public Future<?> start() throws Exception {
 
     Router router = Router.router(vertx);
-    ConfigRetriever retriever = ConfigRetriever.create(vertx, options);
 
-    retriever.configStream().handler(c ->{
-      c.getJsonObject("server").getString("host");
-    });
-
-    Future<JsonObject> future = retriever.getConfig();
-
-
-    Future<JsonObject> f = retriever.getConfig();
-    f.onComplete(ar -> {
-      if (ar.failed()) {
-        // Failed to retrieve the configuration
-      } else {
-        JsonObject config = ar.result();
-        System.out.println(config);
-      }
-    });
-
-
-
+    ConfigStoreOptions configStoreOptions = new ConfigStoreOptions().setType("file").setFormat("json").setConfig(new JsonObject().put("path", "config.json"));
+    ConfigRetriever retriever = ConfigRetriever.create(vertx, new ConfigRetrieverOptions().addStore(configStoreOptions));
 
     router.route().handler(BodyHandler.create());
-    RsaUtil rsaUtil= new RsaUtil();
+    RsaUtil rsaUtil = new RsaUtil();
 
+    router.get("/ping").handler(rc -> {
+      retriever.getConfig().onSuccess(config -> {
+        String message = config.getString("message");
+        rc.request().response().putHeader("content-type", "text/plain").end(message);
+      }).onFailure(err -> rc.fail(500));
+    })
+
+
+    ;
     router.post("/crypto/encryptRSA").handler(rc -> {
-      EncryptableMessage e= rc.body().asJsonObject().mapTo(EncryptableMessage.class);
+      EncryptableMessage e = rc.body().asJsonObject().mapTo(EncryptableMessage.class);
       String ciphertext = rsaUtil.encrypt(e.message);
-      rc.response().putHeader("content-type","text/plain").end(ciphertext);
+      rc.response().putHeader("content-type", "text/plain").end(ciphertext);
     });
 
     router.post("/crypto/decryptRSA").handler(rc -> {
-      EncryptableMessage e= rc.body().asJsonObject().mapTo(EncryptableMessage.class);
+      EncryptableMessage e = rc.body().asJsonObject().mapTo(EncryptableMessage.class);
       String cleartext = rsaUtil.decrypt(e.message);
-      rc.response().putHeader("content-type","text/plain").end("Cleartext:  "+cleartext);
+      rc.response().putHeader("content-type", "text/plain").end("Cleartext:  " + cleartext);
     });
 
-    vertx.createHttpServer()
-      .requestHandler(router)
-      .listen(8080)
-      .onSuccess(server -> startPromise.complete())
-      .onFailure(startPromise::fail);
+
+    return retriever.getConfig().compose(cfg -> {
+      return vertx.createHttpServer().requestHandler(router).listen(cfg.getInteger("port"));
+    });
+
   }
 }
